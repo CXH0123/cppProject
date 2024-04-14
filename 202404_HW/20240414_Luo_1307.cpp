f9053824e4148e3adac1b103ea3274db6becd1bf
CXH <341871976@qq.com>
2024-04-14 14:47:46 +0800

    homework plus

--- 202404_HW/20240414_Luo_1307.cpp
+++ 202404_HW/20240414_Luo_1307.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main(){
     int x=0,n,a[1000];
+    bool f=0;
     cin>>n;
-    if(n<10&&n>=0){
+    if(n==0){
         cout<<n;
         return 0;
     }
@@ -16,7 +17,9 @@ int main(){
         x++;n/=10;
     }
     for(int i=0;i<x;i++){
-        if(a[i]!=0) cout<<a[i];
+        if(a[i]!=0||f){ 
+            cout<<a[i];f=1;
+        }
     }
     return 0;
 }
