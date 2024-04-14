c4620c4ee6436bd6101a04a78611baa4fdce2f5c
CXH <341871976@qq.com>
2024-04-14 14:35:24 +0800

    homework

---
+++ 202404_HW/20240414_Luo_1307.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0,n,a[1000];
+    cin>>n;
+    if(n<10&&n>=0){
+        cout<<n;
+        return 0;
+    }
+    if(n<0){
+        cout<<"-";
+        n=abs(n);
+    }
+    while(n!=0){
+        a[x]=n%10;
+        x++;n/=10;
+    }
+    for(int i=0;i<x;i++){
+        if(a[i]!=0) cout<<a[i];
+    }
+    return 0;
+}
