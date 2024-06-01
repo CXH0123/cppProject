3684182e1613bd7e4873b6c065a6330eaaa21601
CXH <341871976@qq.com>
2024-06-01 14:40:22 +0800

    c++

--- Luo/1031.cpp
+++ Luo/1031.cpp
@@ -1,78 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e4 + 5;
-int a[N];
-bool isRight(int n, int s) {
-    for (int i = 0; i < n; i++) {
-        if (a[i] != s) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-
+int a[101];
 int main() {
     int n, s = 0, ans = 0;
 
     cin >> n;
-    int t = n;
-
     for (int i = 0; i < n; i++) {
         cin >> a[i];
         s += a[i];
     }
     s /= n;
-    while (!isRight(n, s)) {
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (a[i] > s) {
-                    ans++;
-                    a[i + 1] += (a[i] - s);
-                    a[i] -= (a[i] - s);
-                }
-            }else if (i == n - 1) {
-                if (a[i] > s) {
-                    ans++;
-                    a[i - 1] += (a[i] - s);
-                    a[i] -= (a[i] - s);
-                }
-            }else if (a[i] > s) {
-                ans++;
-                if (a[i - 1] < a[i + 1]) {
-                    if (a[i] - s > a[i - 1] - s) {
-                        a[i] -= (a[i - 1] - s);
-                        a[i - 1] = s;
-                    }else{
-                        a[i - 1] += (a[i] - s);
-                        a[i] = s;
-                    }
-                    if (a[i]!=s&&a[i] - s > a[i + 1] - s) {
-                        a[i] -= (a[i + 1] - s);
-                        a[i + 1] = s;
-                    }else{
-                        a[i + 1] += (a[i] - s);
-                        a[i] = s;
-                    }
-                }else{
-                    if (a[i] - s > a[i + 1] - s) {
-                        a[i] -= (a[i + 1] - s);
-                        a[i + 1] = s;
-                    }else{
-                        a[i + 1] += (a[i] - s);
-                        a[i] = s;
-                    }
-                    if (a[i]!=s&&a[i] - s > a[i - 1] - s) {
-                        a[i] -= (a[i - 1] - s);
-                        a[i - 1] = s;
-                    }else{
-                        a[i - 1] += (a[i] - s);
-                        a[i] = s;
-                    }
-                }
-            }
+    for (int i = 0; i < n; i++) {
+        if (a[i] < s) {
+            ans++;
+            a[i + 1] -= (s - a[i]);
+            a[i] = s;
+        }else if (a[i] > s) {
+            ans++;
+            a[i + 1] += (a[i] - s);
+            a[i] = s;
         }
     }
-    cout<<ans;
+    cout << ans;
     return 0;
 }
