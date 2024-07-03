5d5a6c06627ad9372270b1d9b8e476da408a92fc
CXH <341871976@qq.com>
2024-07-03 15:21:46 +0800

    shit c++

---
+++ Luo_2024_6/B3629.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int l=0,r,n;
+    cin>>n;
+    r=n;
+    while(l<r){
+        int mid=l+r>>1;
+        int sum=mid,s=mid,t;
+        while(sum>2){
+            t=sum/3;
+            s+=t;
+            sum-=t*3;
+            sum+=t;
+        }
+        if(s>=n) r=mid;
+        else l=mid+1;
+    }
+    cout<<l;
+    return 0;
+}
