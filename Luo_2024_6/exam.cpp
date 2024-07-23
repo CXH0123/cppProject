3468a3b05415504ef522039849e390bef7ac5024
CXH <341871976@qq.com>
2024-07-23 11:12:47 +0800

    c++

--- Luo_2024_6/exam.cpp
+++ Luo_2024_6/exam.cpp
@@ -1,11 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-void abc(int n){
-    if(n<1) return;
-    cout<<n<<" ";
-    abc(n-1);
+int n,m,a[10005];
+void dfs(int s,int z){
+    if(s==m){
+        int sum=0;
+        for(int i=0;i<s;i++) sum+=a[i];
+        if(sum==n){
+            for(int i=0;i<s;i++) cout<<a[i]<<" ";
+            cout<<"\n";
+        }
+        return;
+    }
+    for(int i=z;i<n;i++){
+        a[s]=i;
+        dfs(s+1,a[s]);
+    }
 }
 int main(){
-    abc(1000);
+    cin>>n>>m;
+    dfs(0,1);
     return 0;
 }
